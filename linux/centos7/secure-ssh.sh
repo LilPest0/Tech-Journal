3681b3ba1bcc905f02bb0aaf99c16138c33ffb63
Lil Pesto <90120192+LilPest0@users.noreply.github.com>
2022-02-23 18:59:34 -0500

    Update secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -3,4 +3,4 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "all your code goes here"
+echo "ping homepedot.com"
